3539ab22d2c219e734f70d4cf8a34faa56dc4c77
deathcap <deathcap@gmx.co.uk>
2016-04-24 12:07:18 -0700

    Support relative paths to process.env.PWD

--- README.md
+++ README.md
@@ -14,12 +14,18 @@ interactive usage). Available commands:
 * `readlink(path[, cb])`: read symbolic link target using `fs.readlink()`
 * `chmod(path, mode[, cb])`: change file mode permission using `fs.chmod()`
 * `chown(path, uid, gid[, cb])`: change file ownership permission using `fs.chown()`
+* `pwd()`: returns the current value of `process.env.PWD`
+* `cd([path, [, cb]])`: sets the `process.env.PWD` working directory variable to `path` if it exists, or `process.env.HOME` if undefined
 * `ls(path[, cb])`: list directory contents using `fs.readdir()`
 * `mv(oldPath, newPath[, cb])`: rename file using `fs.rename()`
 * `rmdir(path[, cb])`: remove directory using `fs.rmdir()`
 * `stat(path[, cb])`: read file statistics using `fs.lstat()`
 * `ln(path, target[, cb)`: create a symbolic link using `fs.symlink()`
 * `rm(path[, cb])`: remove a file using `fs.unlink()`
+
+All paths can either be absolute if starting with '/', or are otherwise relative to
+`process.env.PWD`. The process working directory (as set by `process.chdir()`) is not
+used by this module, only `process.env.PWD`.
 
 By default, the global object is extended, for convenience. Alternatively, each command
 can be accessed on the shellasync object instead:
--- shellasync.js
+++ shellasync.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const fs = require('fs');
+const path_join = require('path').join;
 
 function defaultCB(err, data) {
   console.log(err, data);
 }
+
+function fixpath(path) {
+  if (path === undefined) return pwd(); // empty is current working directory
+  else if (path.charAt(0) === '/') return path; // absolute
+  else return path_join(pwd(), path); // relative to process.env.PWD
+}
+
+function pwd() {
+  return process.env.PWD;
+}
+
+
 
 function cat(paths, cb) {
   if (!cb) cb = defaultCB;
@@ -23,31 +36,37 @@ function cat(paths, cb) {
       }
     }
 
-    fs.readFile(paths[i], 'utf8', (err, data) => saveData(i, err, data));
+    const path = fixpath(paths[i]);
+
+    fs.readFile(path, 'utf8', (err, data) => saveData(i, err, data));
   }
 }
 
 function readlink(path, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
+
   fs.readlink(path, cb);
 }
 
 function chmod(path, mode, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
 
   fs.chmod(path, mode, cb);
 }
 
 function chown(path, uid, gid, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
 
   fs.chown(path, uid, gid, cb);
 }
 
 function cd(path, cb) {
   if (!cb) cb = defaultCB;
-
-  if (path === undefined) path = process.env.HOME;
+  if (path === undefined) path = process.env.HOME; // cd with no path changes to HOME, not PWD
+  path = fixpath(path);
 
   fs.lstat(path, (err, stats) => {
     if (err) cb(null, err);
@@ -56,43 +75,46 @@ function cd(path, cb) {
   });
 }
 
-function pwd() {
-  return process.env.PWD;
-}
-
 function ls(path, cb) {
   if (!cb) cb = defaultCB;
-  if (path === undefined) path = pwd();
+  path = fixpath(path);
 
   fs.readdir(path, cb);
 }
 
 function mv(oldPath, newPath, cb) {
   if (!cb) cb = defaultCB;
+  oldPath = fixpath(oldPath);
+  newPath = fixpath(newPath);
 
   fs.rename(oldPath, newPath, cb);
 }
 
 function rmdir(path, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
   
   fs.rmdir(path, cb);
 }
 
 function stat(path, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
 
   fs.lstat(path, cb);
 }
 
 function ln(path, target, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
+  target = fixpath(target);
 
   fs.symlink(target, path, cb);
 }
 
 function rm(path, cb) {
   if (!cb) cb = defaultCB;
+  path = fixpath(path);
 
   fs.unlink(path, cb);
 }
